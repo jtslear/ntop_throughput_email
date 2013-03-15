bb6f85cf6b3936e3c27230ddfc386728701ec1e4
john T Skarbek <jtslear@gmail.com>
2013-03-15 16:49:59 -0400

    modify mail method for html messaging

--- get_circuit_bandwidth.rb
+++ get_circuit_bandwidth.rb
@@ -3,16 +3,9 @@
 require 'rubygems'
 require 'net/smtp'
 require 'open-uri'
+require 'mail'
 require './get_epoch_times.rb'
 require './mailer.rb'
-require './our_email.rb'
 
-bob = SupportMailer.new
+bob = Mailer.new
 bob.deliver
-
-#epoch = GetEpochTimes.new
-
-
-#open("http://10.1.1.230:3000/plugins/rrdPlugin?action=arbreq&which=graph&arbfile=throughput&arbiface=eth1&arbip=&start=#{epoch.the_start_time}&end=#{epoch.the_end_time}&counter") { |f|
-#  f.each_line { |line| p line }
-#}
--- mailer.rb
+++ mailer.rb
@@ -2,20 +2,29 @@ class Mailer
   SMTP_SERVER = "zimbra.informed-llc.com"
   SMPT_PORT = 25
   FROM_ADDRESS = "support@informed-llc.com"
+  SUPPORT_EMAIL = "johnskarbek@informed-llc.com"
 
-  def initialize(email)
-    @destination_email = email
-  end
-
-  def deliver
-    Net::SMTP.start(SMTP_SERVER) do |smtp|
-      smtp.send_message text, FROM_ADDRESS, @destination_email
+  def initialize
+    @epoch = GetEpochTimes.new
+    Mail.defaults do
+      delivery_method :smtp, { :address   => SMTP_SERVER,
+        :port      => 25 }
     end
   end
 
-  private
-
-  def text
-    ""
+  def deliver
+    mail = Mail.deliver do
+      to SUPPORT_EMAIL
+      from "Support <#{FROM_ADDRESS}>"
+      subject 'Bandwidth Utilization Report'
+      html_part do
+        content_type 'text/html; charset=UTF-8'
+        body "  <p>The following graphs represent a 12 hour window, starting at 8AM to 8PM for the bandwidth of both of our T1 bundles.</p>
+                <p>Coporate Circuit (our internet and VPN connectivity):</p>
+                <img src=#{"http://10.1.1.230:3000/plugins/rrdPlugin?action=arbreq&which=graph&arbfile=throughput&arbiface=eth2&arbip=&start=#{@epoch.the_start_time}end=#{@epoch.the_end_time}&counter"}>
+                <p>Web Circuit (client facing websites):</p>
+                <img src=#{"http://10.1.1.230:3000/plugins/rrdPlugin?action=arbreq&which=graph&arbfile=throughput&arbiface=eth1&arbip=&start=#{@epoc.the_start_time}&end=#{@epoch.the_end_time}&counter"}> "
+      end
+    end
   end
 end
